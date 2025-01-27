dd12be3b98739b0e1075993f9ae83c71eaa56383
Dang Shengqi <dangsq123@163.com>
2025-01-27 14:27:41 +0800

    add video

--- video-list.js
+++ video-list.js
@@ -23,5 +23,55 @@ let allVideos = [
     id: "vid_4",
     theme: 'poker face: A woman exploring the poker kingdom'
   },
-
+  { 
+    name: "The Wolf",
+    src:"./video/The Wolf.mp4",
+    id:"vid_5",
+    theme: "A man running through a dark, misty forest, being chased by a large black wolf"
+  },
+  { 
+    name: "Levitating",
+    src:"./video/Levitating.mp4",
+    id:"vid_6",
+    theme: "A blonde girl performing on stage in a vaporwave style"
+  },
+  { 
+    name: "We Will Rock You",
+    src:"./video/We Will Rock You.mp4",
+    id:"vid_7",
+    theme: "A young man playing soccer in slums, growing up, and aiming to become a star"
+  },
+  { 
+    name: "Magic",
+    src:"./video/Magic.mp4",
+    id:"vid_8",
+    theme: "A male sorcerer with a magic staff stepping out of a grand castle, summoning the souls from the graveyard"
+  },
+  { 
+    name: "Idol",
+    src:"./video/Idol.mp4",
+    id:"vid_9",
+    theme: "A young girl idol is dancing and singing"
+  },
+  { 
+    name: "Jingle Bells",
+    src:"./video/Jingle Bells.mp4",
+    id:"vid_10",
+    theme: "A snowy day, Santa Claus is delivering gifts to children"
+  },
+  { 
+    name: "Wedding March",
+    src:"./video/Wedding March.mp4",
+    id:"vid_11",
+    theme: "A young woman dressed in a white wedding gown and a young man in a tuxedo, holding a heartwarming wedding ceremony"
+  },
+  
 ];
+// The Wolf: A man running through a dark, misty forest, being chased by a large black wolf
+// Levitating: A blonde girl performing on stage in a vaporwave style
+// Magic: A male sorcerer with a magic staff stepping out of a grand castle, summoning the souls from the graveyard
+// Idol: A young girl idol is dancing and singing
+// Jingle Bells: A snowy day, Santa Claus is delivering gifts to children
+// We Will Rock You: A young man playing soccer in slums, growing up, and aiming to become a star.
+// Wedding March: A young woman dressed in a white wedding gown and a young man in a tuxedo, holding a heartwarming wedding ceremony
+
