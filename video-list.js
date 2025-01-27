941cb9b285298d35dc03f810a85d30614e42829e
Dang Shengqi <dangsq123@163.com>
2025-01-27 16:31:34 +0800

    modify name

--- video-list.js
+++ video-list.js
@@ -1,66 +1,66 @@
 let allVideos = [
   {
-    name: "Believer",
+    name: "Believer - Imagine Dragons",
     src: "./video/believer.mp4",
     id: "vid_1",
     theme: 'Believer: A brave warrior defeats a monster, defending the city'
   },
   {
-    name: "Gods",
+    name: "GODS - NewJeans",
     src: "./video/gods.mp4",
     id: "vid_2",
     theme: 'Gods: A young woman soldier rushed to the battlefield, fought bravely, and won, medieval'
   },
   {
-    name: "One Summer's Day",
+    name: "One Summer's Day - Joe Hisaishi",
     src: "./video/onesummersday.mp4",
     id: "vid_3",
     theme: "One summer's day: a little boy is playing in a field in summer"
   },
   {
-    name: "Poker Face",
+    name: "Poker Face - Lady Gaga",
     src: "./video/pokerface.mp4",
     id: "vid_4",
     theme: 'Poker Face: A woman exploring the poker kingdom'
   },
   { 
-    name: "The Wolf",
+    name: "The Wolf - SIAMES",
     src:"./video/The Wolf.mp4",
     id:"vid_5",
     theme: "The Wolf: A man running through a dark, misty forest, being chased by a large black wolf"
   },
   { 
-    name: "Levitating",
+    name: "Levitating - Dua Lipa",
     src:"./video/Levitating.mp4",
     id:"vid_6",
     theme: "Levitating: A blonde girl performing on stage in a vaporwave style"
   },
   { 
-    name: "We Will Rock You",
+    name: "We Will Rock You - Queen",
     src:"./video/We Will Rock You.mp4",
     id:"vid_7",
     theme: "We Will Rock You: A young man playing soccer in slums, growing up, and aiming to become a star"
   },
   { 
-    name: "Magic",
+    name: "Magic - Mystery Skulls",
     src:"./video/Magic.mp4",
     id:"vid_8",
     theme: "Magic: A male sorcerer with a magic staff stepping out of a grand castle, summoning the souls from the graveyard"
   },
   { 
-    name: "Idol",
+    name: "Idol - YOASOBI",
     src:"./video/Idol.mp4",
     id:"vid_9",
     theme: "Idol: A young girl idol is dancing and singing"
   },
   { 
-    name: "Jingle Bells",
+    name: "Jingle Bells - Love to Sing",
     src:"./video/Jingle Bells.mp4",
     id:"vid_10",
     theme: "Jingle Bells: A snowy day, Santa Claus is delivering gifts to children"
   },
   { 
-    name: "Wedding March",
+    name: "Wedding March - Mendelssohn",
     src:"./video/Wedding March.mp4",
     id:"vid_11",
     theme: "Wedding March: A young woman dressed in a white wedding gown and a young man in a tuxedo, holding a heartwarming wedding ceremony"
