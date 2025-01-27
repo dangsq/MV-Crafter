eadaae4fecb7d8a41d01ecb8c5153348a90eeeeb
Dang Shengqi <dangsq123@163.com>
2025-01-27 15:34:44 +0800

    modify name

--- video-list.js
+++ video-list.js
@@ -1,69 +1,69 @@
 let allVideos = [
   {
-    name: "believer",
+    name: "Believer",
     src: "./video/believer.mp4",
     id: "vid_1",
-    theme: 'believer：A brave warrior defeats a monster, defending the city'
+    theme: 'Believer: A brave warrior defeats a monster, defending the city'
   },
   {
-    name: "gods",
+    name: "Gods",
     src: "./video/gods.mp4",
     id: "vid_2",
-    theme: 'gods: A young woman soldier rushed to the battlefield, fought bravely, and won, medieval'
+    theme: 'Gods: A young woman soldier rushed to the battlefield, fought bravely, and won, medieval'
   },
   {
-    name: "onesummersday",
+    name: "One Summer's Day",
     src: "./video/onesummersday.mp4",
     id: "vid_3",
-    theme: "one summer's day: a little boy is playing in a field in summer"
+    theme: "One summer's day: a little boy is playing in a field in summer"
   },
   {
-    name: "pokerface",
+    name: "Poker Face",
     src: "./video/pokerface.mp4",
     id: "vid_4",
-    theme: 'poker face: A woman exploring the poker kingdom'
+    theme: 'Poker Face: A woman exploring the poker kingdom'
   },
   { 
     name: "The Wolf",
     src:"./video/The Wolf.mp4",
     id:"vid_5",
-    theme: "A man running through a dark, misty forest, being chased by a large black wolf"
+    theme: "The Wolf: A man running through a dark, misty forest, being chased by a large black wolf"
   },
   { 
     name: "Levitating",
     src:"./video/Levitating.mp4",
     id:"vid_6",
-    theme: "A blonde girl performing on stage in a vaporwave style"
+    theme: "Levitating: A blonde girl performing on stage in a vaporwave style"
   },
   { 
     name: "We Will Rock You",
     src:"./video/We Will Rock You.mp4",
     id:"vid_7",
-    theme: "A young man playing soccer in slums, growing up, and aiming to become a star"
+    theme: "We Will Rock You: A young man playing soccer in slums, growing up, and aiming to become a star"
   },
   { 
     name: "Magic",
     src:"./video/Magic.mp4",
     id:"vid_8",
-    theme: "A male sorcerer with a magic staff stepping out of a grand castle, summoning the souls from the graveyard"
+    theme: "Magic: A male sorcerer with a magic staff stepping out of a grand castle, summoning the souls from the graveyard"
   },
   { 
     name: "Idol",
     src:"./video/Idol.mp4",
     id:"vid_9",
-    theme: "A young girl idol is dancing and singing"
+    theme: "Idol: A young girl idol is dancing and singing"
   },
   { 
     name: "Jingle Bells",
     src:"./video/Jingle Bells.mp4",
     id:"vid_10",
-    theme: "A snowy day, Santa Claus is delivering gifts to children"
+    theme: "Jingle Bells: A snowy day, Santa Claus is delivering gifts to children"
   },
   { 
     name: "Wedding March",
     src:"./video/Wedding March.mp4",
     id:"vid_11",
-    theme: "A young woman dressed in a white wedding gown and a young man in a tuxedo, holding a heartwarming wedding ceremony"
+    theme: "Wedding March: A young woman dressed in a white wedding gown and a young man in a tuxedo, holding a heartwarming wedding ceremony"
   },
   
 ];
